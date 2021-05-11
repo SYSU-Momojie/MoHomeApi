e07f5bd01b4436134ffbe636e98dda06c8bd01ec
SYSY-Momojie <1069614543@qq.com>
2021-05-11 17:49:00 +0800

    clean

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mvn clean
 mvn install -Dmaven.test.skip=true
 ps aux|grep mo-app|awk '{print $2}'|xargs kill -9
 nohup java -jar ./app/target/mo-app-1.0.0-SNAPSHOT.jar >> api.log 2>&1 &
